c168bf52ad8eaf34c8ee68ffdd73f5293316d50c
KKambi <37494867+KKambi@users.noreply.github.com>
2019-10-01 14:20:15 +0900

    Add: sass-loader config in webpack.config.js
    - 웹팩 번들링 시, sass compiling을 위해 웹팩설정파일에 추가

--- client/webpack.config.js
+++ client/webpack.config.js
@@ -3,7 +3,16 @@ const path = require('path');
 module.exports = {
   entry: './src/index.js',
   output: {
-    path: path.resolve(__dirname, 'dist'),
+    path: path.resolve(__dirname, 'dist'),  //산출물이 위치할 경로
     filename: 'main.js'
-  }
+  },
+  rules: [
+    {
+      test: /\.sass$/,
+      use: [
+        // Compiles Sass to CSS
+        'sass-loader'
+      ],
+    },
+  ],
 };
