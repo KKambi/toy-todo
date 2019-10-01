4f0b2c600600bade7135f87261efb86e45d7f699
KKambi <37494867+KKambi@users.noreply.github.com>
2019-10-01 14:23:48 +0900

    Add: cleanWebpackPlugin in webpack.config.js
    - 번들링 할 때마다 산출물이 쌓이는 것을 방지하기 위해 클린웹팩플러그인
    설치 및 적용

--- client/webpack.config.js
+++ client/webpack.config.js
@@ -1,18 +1,25 @@
 const path = require('path');
+const CleanWebpackPlugin = require('clean-webpack-plugin');
 
 module.exports = {
-  entry: './src/index.js',
-  output: {
-    path: path.resolve(__dirname, 'dist'),  //산출물이 위치할 경로
-    filename: 'main.js'
-  },
-  rules: [
-    {
-      test: /\.sass$/,
-      use: [
-        // Compiles Sass to CSS
-        'sass-loader'
-      ],
+    entry: {
+        index: './src/index.entry.js'
     },
-  ],
+    output: {
+        path: path.resolve(__dirname, 'dist'),  //산출물이 위치할 경로
+        filename: '[name].bundle.js'
+    },
+    rules: [
+        {
+            test: /\.sass$/,
+            use: [
+                // Compiles Sass to CSS
+                'sass-loader'
+            ],
+        },
+    ],
+    plugins: [
+        new HtmlWebpackPlugin(),
+        new CleanWebpackPlugin(['dist'])
+    ]
 };
